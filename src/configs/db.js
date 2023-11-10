06cb75cf26dcbf4173d4a8d19934aab1682aa85a
Yaroslav <6715414@gmail.com>
2023-11-10 12:07:37 +0200

    feat: add seedDB in connectDB

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,11 +1,22 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const keys = require('./keys')
+const Game = require('../models/Game');
+const keys = require('./keys');
+
+const games = require('../_data/games.json');
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(keys.db.url)
+  const conn = await mongoose.connect(keys.db.url);
 
-  console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold)
-}
+  if (keys.app.mode === 'development') {
+    await seedDB();
+  }
+  console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
+};
 
-module.exports = connectDB
+const seedDB = async () => {
+  await Game.deleteMany({});
+  await Game.insertMany(games);
+};
+
+module.exports = connectDB;
