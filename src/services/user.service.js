2ff8a24bd01b7843f744b84faf8c89303944f976
Yaroslav <6715414@gmail.com>
2023-11-10 12:08:23 +0200

    feat: add user service

---
+++ src/services/user.service.js
@@ -0,0 +1,75 @@
+const bcrypt = require('bcrypt');
+
+const UserModel = require('../models/User');
+const UserDto = require('../dtos/user.dto');
+const ApiError = require('../utils/exceptions/app.error');
+const tokenService = require('./token.service');
+
+class UserService {
+  async signUp(email, password) {
+    const existedUser = await UserModel.findOne({ email });
+
+    if (existedUser) {
+      throw ApiError.BadRequest(`User already exists`);
+    }
+
+    const hashPassword = await bcrypt.hash(password, 3);
+
+    const user = await UserModel.create({
+      email,
+      password: hashPassword,
+    });
+    const userDto = new UserDto(user);
+
+    const tokens = tokenService.generateTokens({ ...userDto });
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+    return { ...tokens, user: userDto };
+  }
+
+  async signIn(email, password) {
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      throw ApiError.BadRequest(`User does not exists`);
+    }
+
+    const isPassEquals = await bcrypt.compare(password, user.password);
+    if (!isPassEquals) {
+      throw ApiError.BadRequest('Email or password is not correct');
+    }
+
+    const userDto = new UserDto(user);
+    const tokens = tokenService.generateTokens({ ...userDto });
+
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+    return { ...tokens, user: userDto };
+  }
+
+  async logout(refreshToken) {
+    const token = await tokenService.removeToken(refreshToken);
+    return token;
+  }
+
+  async refresh(refreshToken) {
+    if (!refreshToken) {
+      throw ApiError.UnauthorizedError();
+    }
+
+    const userData = tokenService.validateRefreshToken(refreshToken);
+    const tokenFromDb = await tokenService.findToken(refreshToken);
+
+    if (!userData || !tokenFromDb) {
+      throw ApiError.UnauthorizedError();
+    }
+
+    const user = await UserModel.findById(userData.id);
+    const userDto = new UserDto(user);
+
+    const tokens = tokenService.generateTokens({ ...userDto });
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+    return { ...tokens, user: userDto };
+  }
+}
+
+module.exports = new UserService();
