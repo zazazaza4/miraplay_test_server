10e41c9d83fc279f2090c9b62add722ea3d1cd7f
Yaroslav <6715414@gmail.com>
2023-11-10 10:00:14 +0200

    feat: auth middleware

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,27 @@
+const ApiError = require('../utils/exceptions/app.error');
+
+const auth = (req, res, next) => {
+  try {
+    const authorizationHeader = req.headers.authorization;
+    if (!authorizationHeader) {
+      return next(ApiError.UnauthorizedError());
+    }
+
+    const accessToken = authorizationHeader.split(' ')[1];
+    if (!accessToken) {
+      return next(ApiError.UnauthorizedError());
+    }
+
+    const userData = tokenService.validateAccessToken(accessToken);
+    if (!userData) {
+      return next(ApiError.UnauthorizedError());
+    }
+
+    req.user = userData;
+    next();
+  } catch (e) {
+    return next(ApiError.UnauthorizedError());
+  }
+};
+
+module.exports = auth;
