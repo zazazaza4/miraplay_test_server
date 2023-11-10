ef096d0630e60f346fc83b7aa3634c76203eff40
Yaroslav <6715414@gmail.com>
2023-11-10 11:01:18 +0200

    feat: add auth controller

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,79 @@
+const bcrypt = require('bcrypt');
+
+const userService = require('../services/user.service');
+const ApiError = require('../utils/exceptions/app.error');
+
+class AuthController {
+  async register(req, res) {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return next(
+          ApiError.BadRequest(
+            'Bad request. Please try again with valid parameters',
+            errors.array()
+          )
+        );
+      }
+
+      const { email, password } = req.body;
+      const userData = await userService.registration(email, password);
+
+      res.cookie('refreshToken', userData.refreshToken, {
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+      });
+
+      return res.json(userData);
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async login(req, res) {
+    try {
+      const { email, password } = req.body;
+      const userData = await userService.login(email, password);
+
+      res.cookie('refreshToken', userData.refreshToken, {
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+      });
+
+      return res.json(userData);
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async logout(req, res, next) {
+    try {
+      const { refreshToken } = req.cookies;
+      const token = await userService.logout(refreshToken);
+
+      res.clearCookie('refreshToken');
+
+      return res.json(token);
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async refresh(req, res, next) {
+    try {
+      const { refreshToken } = req.cookies;
+      const userData = await userService.refresh(refreshToken);
+
+      res.cookie('refreshToken', userData.refreshToken, {
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+      });
+
+      return res.json(userData);
+    } catch (e) {
+      next(e);
+    }
+  }
+}
+
+module.exports = new AuthController();
