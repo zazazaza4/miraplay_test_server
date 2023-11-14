8506715e55604f150d5cbaf9af608393a811e613
Yaroslav <6715414@gmail.com>
2023-11-14 21:03:49 +0200

    refactor: change routes

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,13 +1,10 @@
-const router = require('express').Router()
+const router = require('express').Router();
 
-const authRoutes = require('./auth.routes')
-const userRoutes = require('./user.routes')
-const gameRoutes = require('./game.routes')
+const authRoutes = require('./auth.routes');
+const gameRoutes = require('./game.routes');
 
-router.use('/auth', authRoutes)
+router.use('/auth', authRoutes);
 
-router.use('/user', userRoutes)
+router.use('/games', gameRoutes);
 
-router.use('/game', gameRoutes)
-
-module.exports = router
+module.exports = router;
