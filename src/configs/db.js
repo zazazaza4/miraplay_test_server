660fcad38c3169d97d2e7a845ec2547fad26ee2e
Yaroslav <6715414@gmail.com>
2023-11-15 03:08:40 +0200

    refactor: add Error Handling and Connection Options to db

--- src/configs/db.js
+++ src/configs/db.js
@@ -6,12 +6,21 @@ const keys = require('./keys');
 const games = require('../_data/games.json');
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(keys.db.url);
+  try {
+    const conn = await mongoose.connect(keys.db.url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-  if (keys.app.mode === 'development') {
-    await seedDB();
+    if (keys.app.mode === 'development') {
+      await seedDB();
+    }
+    console.log(
+      `MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold
+    );
+  } catch (error) {
+    console.error(`Error connecting to MongoDB: ${error.message}`.red.bold);
   }
-  console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
 };
 
 const seedDB = async () => {
